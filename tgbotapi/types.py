acb01852e0d8dc477f6c70c86309ad8396b7f57d
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-27 18:26:24 +0300

    Add Animation object

--- tgbotapi/types.py
+++ tgbotapi/types.py
@@ -85,3 +85,51 @@ class JsonSerializable(object):
         :return: a JSON formatted string.
         """
         raise NotImplementedError
+
+
+class Animation(JsonDeserializable):
+    """ This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound) 
+        :param file_id: [STRING] Identifier for this file, which can be used to download or reuse the file.
+        :param file_unique_id: [STRING] Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file..
+        :param width: [INTEGER] Video width as defined by sender.
+        :param height: [INTEGER] Video height as defined by sender.
+        :param duration: [INTEGER] Duration of the video in seconds as defined by sender.
+        :param thumb: [PhotoSize] Optional. Animation thumbnail as defined by sender.
+        :param file_name: [STRING] Optional. Original animation filename as defined by sender.
+        :param mime_type: [STRING] Optional. MIME type of the file as defined by sender.
+        :param file_size: [INTEGER] Optional. File size.
+        :return JSON_OBJECT:
+    """
+
+    def __init__(self, file_id, file_unique_id, width, height, duration, thumb, file_name, mime_type, file_size):
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.width = width
+        self.height = height
+        self.duration = duration
+        self.thumb = thumb
+        self.file_name = file_name
+        self.mime_type = mime_type
+        self.file_size = file_size
+
+    @classmethod
+    def de_json(cls, json_string):
+        obj = cls.check_json(json_string)
+        file_id = obj['file_id']
+        file_unique_id = obj['file_unique_id']
+        width = obj['width']
+        height = obj['height']
+        duration = obj['duration']
+        thumb = None
+        if 'thumb' in obj:
+            thumb = PhotoSize.de_json(obj['thumb'])
+        file_name = None
+        if 'file_name' in obj:
+            file_name = obj.get('file_name')
+        mime_type = None
+        if 'mime_type' in obj:
+            mime_type = obj.get('mime_type')
+        file_size = None
+        if 'file_size' in obj:
+            file_size = obj.get('file_size')
+        return cls(file_id, file_unique_id, width, height, duration, thumb, file_name, mime_type, file_size)
