cab86b3872d0b2636c4435f101c8e4ae4d8f32fb
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-27 18:21:41 +0300

    Add Dict class for string

---
+++ tgbotapi/types.py
@@ -0,0 +1,21 @@
+""" Telegram Available methods
+    All methods in the Bot API are case-insensitive. We support GET and POST HTTP methods. 
+    Use either URL query string or application/json or application/x-www-form-urlencoded or multipart/form-data for passing parameters in Bot API requests.
+    On successful call, a JSON-object containing the result will be returned.
+"""
+
+
+class Dictionaryable(object):
+    """
+    Subclasses of this class are guaranteed to be able to be converted to dictionary,
+    All subclasses of this class must override to_dic.
+    """
+
+    def to_dic(self):
+        """
+        Returns a JSON string representation of this class.
+
+        This function must be overridden by subclasses.
+        :return: a JSON formatted string.
+        """
+        raise NotImplementedError
