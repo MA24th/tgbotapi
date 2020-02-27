2c892c118bec54aec15807300325f408c7c1e5ec
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-27 18:15:26 +0300

    Add make setup package

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from io import open
+
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+
+setup(name='tgbotapi',
+      version='1.0.0',
+      description='The Ultimate Telegram Bot API Client Framework',
+      long_description=read('README.rst'),
+      long_description_content_type="text/x-rst",
+      author='Mustafa Asaad',
+      author_email='ma24th@yahoo.com',
+      url='https://github.com/MA24th/tgbotapi',
+      packages=find_packages(),
+      license='GNU GPLv2',
+      keywords='telegram-bot-api, tgbotapi, framework, telegram bot api, bot api',
+      install_requires=['requests', 'six'],
+      extras_require={'json': 'json'},
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Programming Language :: Python :: 3.7',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)']
+      )
