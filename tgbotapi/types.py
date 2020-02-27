4a219beb9c81510c2c4b39672c2f29cae8feff92
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-27 18:25:19 +0300

    Add json object mapper

--- tgbotapi/types.py
+++ tgbotapi/types.py
@@ -1,3 +1,6 @@
+import json
+import six
+
 """ Telegram Available methods
     All methods in the Bot API are case-insensitive. We support GET and POST HTTP methods. 
     Use either URL query string or application/json or application/x-www-form-urlencoded or multipart/form-data for passing parameters in Bot API requests.
@@ -12,6 +15,69 @@ class Dictionaryable(object):
     """
 
     def to_dic(self):
+        """
+        Returns a JSON string representation of this class.
+
+        This function must be overridden by subclasses.
+        :return: a JSON formatted string.
+        """
+        raise NotImplementedError
+
+
+class JsonDeserializable(object):
+    """
+    Subclasses of this class are guaranteed to be able to be created from a json-style dict or json formatted string,
+    All subclasses of this class must override de_json.
+    """
+
+    @classmethod
+    def de_json(cls, json_type):
+        """
+        Returns an instance of this class from the given json dict or string.
+
+        This function must be overridden by subclasses.
+        :return: an instance of this class created from the given json dict or string.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def check_json(json_type):
+        """
+        Checks whether json_type is a dict or a string. If it is already a dict, it is returned as-is,
+        If it is not, it is converted to a dict by means of json.loads(json_type),
+        :param json_type:
+        :return:
+        """
+        try:
+            str_types = (str, unicode)
+        except NameError:
+            str_types = (str,)
+
+        if type(json_type) == dict:
+            return json_type
+        elif type(json_type) in str_types:
+            return json.loads(json_type)
+        else:
+            raise ValueError("json_type should be a json dict or string.")
+
+    def __str__(self):
+        d = {}
+        for x, y in six.iteritems(self.__dict__):
+            if hasattr(y, '__dict__'):
+                d[x] = y.__dict__
+            else:
+                d[x] = y
+
+        return six.text_type(d)
+
+
+class JsonSerializable(object):
+    """
+    Subclasses of this class are guaranteed to be able to be converted to JSON format,
+    All subclasses of this class must override to_json.
+    """
+
+    def to_json(self):
         """
         Returns a JSON string representation of this class.
 
