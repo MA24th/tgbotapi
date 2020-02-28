9a6915e30bfed8a878e7ee6034beb8fa5b45e4d4
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-28 18:29:19 +0300

    Add User object

--- tgbotapi/types.py
+++ tgbotapi/types.py
@@ -88,7 +88,10 @@ class JsonSerializable(object):
 
 
 class Animation(JsonDeserializable):
-    """ This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound) 
+    """ This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound) """
+
+    def __init__(self, file_id, file_unique_id, width, height, duration, thumb, file_name, mime_type, file_size):
+        """
         :param file_id: [STRING] Identifier for this file, which can be used to download or reuse the file.
         :param file_unique_id: [STRING] Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file..
         :param width: [INTEGER] Video width as defined by sender.
@@ -98,10 +101,7 @@ class Animation(JsonDeserializable):
         :param file_name: [STRING] Optional. Original animation filename as defined by sender.
         :param mime_type: [STRING] Optional. MIME type of the file as defined by sender.
         :param file_size: [INTEGER] Optional. File size.
-        :return JSON-object:
-    """
-
-    def __init__(self, file_id, file_unique_id, width, height, duration, thumb, file_name, mime_type, file_size):
+        """
         self.file_id = file_id
         self.file_unique_id = file_unique_id
         self.width = width
@@ -114,6 +114,7 @@ class Animation(JsonDeserializable):
 
     @classmethod
     def de_json(cls, json_string):
+        """ :return JSON-object """
         obj = cls.check_json(json_string)
         file_id = obj['file_id']
         file_unique_id = obj['file_unique_id']
@@ -136,16 +137,17 @@ class Animation(JsonDeserializable):
 
 
 class PhotoSize(JsonDeserializable):
-    """ This object represents one size of a photo or a file / sticker thumbnail 
+    """ This object represents one size of a photo or a file / sticker thumbnail """
+
+    def __init__(self, file_id, file_unique_id, width, height, file_size=None):
+        """
         :param file_id: [STRING] Identifier for this file, which can be used to download or reuse the file.
         :param file_unique_id: [STRING] Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file..
         :param width: [INTEGER] Photo width.
         :param height: [INTEGER] Phot height.
         :param file_size: [INTEGER] Optional. File size.
-        :return JSON-object:
-    """
+        """
 
-    def __init__(self, file_id, file_unique_id, width, height, file_size=None):
         self.file_id = file_id
         self.file_unique_id = file_unique_id
         self.width = width
@@ -154,6 +156,7 @@ class PhotoSize(JsonDeserializable):
 
     @classmethod
     def de_json(cls, json_string):
+        """ :return JSON-object """
         obj = cls.check_json(json_string)
         file_id = obj['file_id']
         file_unique_id = obj['file_unique_id']
@@ -161,3 +164,58 @@ class PhotoSize(JsonDeserializable):
         height = obj['height']
         file_size = obj.get('file_size')
         return cls(file_id, file_unique_id, width, height, file_size)
+
+
+class User(JsonDeserializable):
+    """ This object represents a Telegram user or bot """
+
+    def __init__(self, id, is_bot, first_name, last_name=None, username=None, language_code=None, can_join_groups=None, can_read_all_group_messages=None, supports_inline_queries=None):
+        """
+        :param id: [Integer] Unique identifier for this user or bot.
+        :param is_bot: [Boolean] True, if this user is a bot.
+        :param first_name: [String] User‘s or bot’s first name.
+        :param last_name: [String] Optional. User‘s or bot’s last name.
+        :param username: [String] Optional. User‘s or bot’s username.
+        :param language_code: [String] Optional. IETF language tag of the user's language.
+        :param can_join_groups: [Boolean] Optional. True, if the bot can be invited to groups. Returned only in getMe.
+        :param can_read_all_group_messages: [Boolean] Optional. True, if privacy mode is disabled for the bot. Returned only in getMe.
+        :param supports_inline_queries: [Boolean] Optional. True, if the bot supports inline queries. Returned only in getMe.
+        """
+
+        self.id = id
+        self.is_bot = is_bot
+        self.first_name = first_name
+        self.username = username
+        self.last_name = last_name
+        self.language_code = language_code
+        self.can_join_groups = can_join_groups
+        self.can_read_all_group_messages = can_read_all_group_messages
+        self.supports_inline_queries = supports_inline_queries
+
+    @classmethod
+    def de_json(cls, json_string):
+        """ :return JSON-object """
+        obj = cls.check_json(json_string)
+        id = obj['id']
+        is_bot = obj['is_bot']
+        first_name = obj['first_name']
+        last_name = None
+        if 'last_name' in obj:
+            last_name = obj.get('last_name')
+        username = None
+        if 'username' in obj:
+            username = obj.get('username')
+        language_code = None
+        if 'language_code' in obj:
+            language_code = obj.get('language_code')
+        can_join_groups = None
+        if 'can_join_groups' in obj:
+            can_join_groups = obj.get('can_join_groups')
+        can_read_all_group_messages = None
+        if 'can_read_all_group_messages' in obj:
+            can_read_all_group_messages = obj.get(
+                'can_read_all_group_messages')
+        supports_inline_queries = None
+        if 'supports_inline_queries' in obj:
+            supports_inline_queries = obj.get('supports_inline_queries')
+        return cls(id, is_bot, first_name, last_name, username, language_code, can_join_groups, can_read_all_group_messages, supports_inline_queries)
