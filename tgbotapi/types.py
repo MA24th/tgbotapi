dc9b8ce2fc2c9b6e50600cd3da0f4b4e78577df3
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-28 20:26:50 +0300

    Add ReplyKbdRemove, InlineKbdMkup & InlineKbdBtn

--- tgbotapi/types.py
+++ tgbotapi/types.py
@@ -1116,7 +1116,7 @@ class KeyboardButton(Dictionaryable, JsonSerializable):
         if self.request_location:
             json_dic['request_location'] = self.request_location
         if self.request_poll:
-            json_dic['request_poll'] = keyboardButtonPollType(
+            json_dic['request_poll'] = KeyboardButtonPollType(
                 self.request_poll)
         return json_dic
 
@@ -1132,3 +1132,117 @@ class KeyboardButtonPollType(JsonDeserializable):
 
     def __init__(self, poll_type):
         self.poll_type = poll_type
+
+
+class ReplyKeyboardRemove(JsonSerializable):
+    """ 
+    Upon receiving a message with this object, 
+    Telegram clients will remove the current custom keyboard and display the default letter-keyboard,
+    By default, custom keyboards are displayed until a new keyboard is sent by a bot,
+    An exception is made for one-time keyboards that are hidden immediately after the user presses a button (see ReplyKeyboardMarkup).
+    """
+
+    def __init__(self, selective=None):
+        self.selective = selective
+
+    def to_json(self):
+        json_dict = {'remove_keyboard': True}
+        if self.selective:
+            json_dict['selective'] = True
+        return json.dumps(json_dict)
+
+
+class InlineKeyboardMarkup(Dictionaryable, JsonSerializable):
+    """ This object represents an inline keyboard that appears right next to the message it belongs to. """
+
+    def __init__(self, row_width=3):
+        self.row_width = row_width
+
+        self.keyboard = []
+
+    def add(self, *args):
+        """
+        This function adds strings to the keyboard, while not exceeding row_width.
+        E.g. ReplyKeyboardMarkup#add("A", "B", "C") yields the json result {keyboard: [["A"], ["B"], ["C"]]}
+        when row_width is set to 1.
+        When row_width is set to 2, the following is the result of this function: {keyboard: [["A", "B"], ["C"]]}
+        See https://core.telegram.org/bots/api#replykeyboardmarkup
+        :param args: KeyboardButton to append to the keyboard
+        """
+        i = 1
+        row = []
+        for button in args:
+            row.append(button.to_dic())
+            if i % self.row_width == 0:
+                self.keyboard.append(row)
+                row = []
+            i += 1
+        if len(row) > 0:
+            self.keyboard.append(row)
+
+    def row(self, *args):
+        """
+        Adds a list of KeyboardButton to the keyboard. This function does not consider row_width.
+        ReplyKeyboardMarkup#row("A")#row("B", "C")#to_json() outputs '{keyboard: [["A"], ["B", "C"]]}'
+        See https://core.telegram.org/bots/api#inlinekeyboardmarkup
+        :param args: strings
+        :return: self, to allow function chaining.
+        """
+        btn_array = []
+        for button in args:
+            btn_array.append(button.to_dic())
+        self.keyboard.append(btn_array)
+        return self
+
+    def to_json(self):
+        """
+        Converts this object to its json representation following the Telegram API guidelines described here:
+        https://core.telegram.org/bots/api#inlinekeyboardmarkup
+        :return:
+        """
+        json_dict = {'inline_keyboard': self.keyboard}
+        return json.dumps(json_dict)
+
+    def to_dic(self):
+        json_dict = {'inline_keyboard': self.keyboard}
+        return json_dict
+
+
+class InlineKeyboardButton(JsonSerializable):
+    """ 
+    This object represents one button of an inline keyboard, 
+    You must use exactly one of the optional fields.
+    """
+
+    def __init__(self, text, url=None, callback_data=None, switch_inline_query=None,
+                 switch_inline_query_current_chat=None, callback_game=None, pay=None, login_url=None):
+        self.text = text
+        self.url = url
+        self.login_url = login_url
+        self.callback_data = callback_data
+        self.switch_inline_query = switch_inline_query
+        self.switch_inline_query_current_chat = switch_inline_query_current_chat
+        self.callback_game = callback_game
+        self.pay = pay
+
+    def to_json(self):
+        return json.dumps(self.to_dic())
+
+    def to_dic(self):
+        json_dic = {'text': self.text}
+        if self.url:
+            json_dic['url'] = self.url
+        if self.callback_data:
+            json_dic['callback_data'] = self.callback_data
+        if self.switch_inline_query is not None:
+            json_dic['switch_inline_query'] = self.switch_inline_query
+        if self.switch_inline_query_current_chat is not None:
+            json_dic['switch_inline_query_current_chat'] = self.switch_inline_query_current_chat
+        if self.callback_game is not None:
+            json_dic['callback_game'] = self.callback_game
+        if self.pay is not None:
+            json_dic['pay'] = self.pay
+        if self.login_url is not None:
+            json_dic['login_url'] = self.login_url
+        return json_dic
+
