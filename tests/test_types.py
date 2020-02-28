ca499b6bc719cd5d729e918a06748476e51fbb19
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-28 14:34:26 +0300

    Add test for Animation object

---
+++ tests/test_types.py
@@ -0,0 +1,33 @@
+import pytest
+from tgbotapi import types
+
+
+def test_Animation():
+    dic = {
+        "file_id": 1,
+        "file_unique_id": 221,
+        "width": 60,
+        "height": 60,
+        "duration": 53,
+        "thumb": {
+            "file_id": 12,
+            "file_unique_id": 22,
+            "width": 40,
+            "height": 40},
+        "file_name": "filename",
+        "mime_type": "type",
+        "file_size": 64
+    }
+    obj = types.Animation.de_json(dic)
+    assert obj.file_id == 1
+    assert obj.file_unique_id == 221
+    assert obj.width == 60
+    assert obj.height == 60
+    assert obj.duration == 53
+    assert obj.thumb.file_id == 12
+    assert obj.thumb.file_unique_id == 22
+    assert obj.thumb.width == 40
+    assert obj.thumb.height == 40
+    assert obj.file_name == 'filename'
+    assert obj.mime_type == 'type'
+    assert obj.file_size == 64
