5688258298f3dc0a93f5387a52bf0b12485d3cae
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-02-29 17:41:33 +0300

    Add Saver for saving handlers

--- tgbotapi/__init__.py
+++ tgbotapi/__init__.py
@@ -1,3 +1,7 @@
+import threading
+import pickle
+import os
+
 """
 Module : tgbotapi
 """
@@ -15,3 +19,56 @@ class Handler:
 
     def __getitem__(self, item):
         return getattr(self, item)
+
+
+class Saver:
+    """
+    Class for saving (next step|reply) handlers
+    """
+
+    def __init__(self, handlers, filename, delay):
+        self.handlers = handlers
+        self.filename = filename
+        self.delay = delay
+        self.timer = threading.Timer(delay, self.save_handlers)
+
+    def start_save_timer(self):
+        if not self.timer.is_alive():
+            if self.delay <= 0:
+                self.save_handlers()
+            else:
+                self.timer = threading.Timer(self.delay, self.save_handlers)
+                self.timer.start()
+
+    def save_handlers(self):
+        self.dump_handlers(self.handlers, self.filename)
+
+    def load_handlers(self, filename, del_file_after_loading=True):
+        tmp = self.return_load_handlers(
+            filename, del_file_after_loading=del_file_after_loading)
+        if tmp is not None:
+            self.handlers.update(tmp)
+
+    @staticmethod
+    def dump_handlers(handlers, filename, file_mode="wb"):
+        dirs = filename.rsplit('/', maxsplit=1)[0]
+        os.makedirs(dirs, exist_ok=True)
+
+        with open(filename + ".tmp", file_mode) as file:
+            pickle.dump(handlers, file)
+
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        os.rename(filename + ".tmp", filename)
+
+    @staticmethod
+    def return_load_handlers(filename, del_file_after_loading=True):
+        if os.path.isfile(filename) and os.path.getsize(filename) > 0:
+            with open(filename, "rb") as file:
+                handlers = pickle.load(file)
+
+            if del_file_after_loading:
+                os.remove(filename)
+
+            return handlers
