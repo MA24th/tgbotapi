262f812224d9fd2e24d128fcb3f45a9ab22a12ec
Mustafa Asaad <ma24th@yahoo.com>
2022-03-08 21:56:25 +0300

    Added support for the choose_sticker action in the method sendChatAction.

--- tgbotapi/bot.py
+++ tgbotapi/bot.py
@@ -940,7 +940,11 @@ class Bot:
         """
         Use this method when you need to tell the user that something is happening on the bots side
         :param int or str chat_id: Unique identifier for the target chat or username of the target channel
-        :param str action: Type of action to broadcast
+        :param str action: Type of action to broadcast. Choose one, depending on what the user is about to receive:
+                           typing for text messages, upload_photo for photos, record_video or upload_video for videos,
+                           record_voice or upload_voice for voice notes, upload_document for general files,
+                           choose_sticker for stickers, find_location for location data, record_video_note or
+                           upload_video_note for video notes
         :return: True On success
         :rtype: bool
         """
