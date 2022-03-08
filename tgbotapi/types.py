df707a94ada76f6197d799059e28880cb7645b9c
Mustafa Asaad <ma24th@yahoo.com>
2022-03-08 23:39:31 +0300

    Added new MessageEntity type “spoiler”.

--- tgbotapi/types.py
+++ tgbotapi/types.py
@@ -484,7 +484,12 @@ class MessageEntity(Dictionaryable, JsonDeserializable):
     def __init__(self, ttype, offset, length, url, user, language):
         """
         This object represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
-        :param str ttype: Type of the entity
+        :param str ttype: Type of the entity. Currently, can be “mention” (@username), “hashtag” (#hashtag), “cashtag”
+                         ($USD), “bot_command” (/start@jobs_bot), “url” (https://telegram.org),
+                         “email” (do-not-reply@telegram.org), “phone_number” (+1-212-555-0123), “bold” (bold text),
+                         “italic” (italic text), “underline” (underlined text), “strikethrough” (strikethrough text),
+                         “spoiler” (spoiler message), “code” (mono-width string), “pre” (mono-width block),
+                         “text_link” (for clickable text URLs), “text_mention” (for users without usernames)
         :param int offset: Offset in UTF-16 code units to the start of the entity
         :param int length: Length of the entity in UTF-16 code units
         :param str or None url: Optional. For “text_link” only, url that will be opened after user taps on the text
