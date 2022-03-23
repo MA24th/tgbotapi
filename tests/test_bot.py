82a964f1cd5fbe48f92fd046fc5e2504da4e8a05
Mustafa Asaad <ma24th@yahoo.com>
2022-03-23 13:15:18 +0300

    Rename

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,28 +1,28 @@
 import os
 import logging
 from tgbotapi import logger
-from tgbotapi import Bot as Bt
+from tgbotapi import Bot
 
 logger.setLevel(logging.DEBUG)
-bot = Bt(based_url="https://api.telegram.org/bot" + os.getenv("BOT_TOKEN"), allowed_updates=['message'],
-         threaded=True, skip_pending=False, num_threads=200, proxies=None)
+
+bot = Bot(access_token=os.getenv("BOT_TOKEN"))
 
 
-@bot.update_handler(update_type='message', bot_command=['start', 'help'])
+@bot.update_handler(bot_command=['/start', '/help'])
 def send_welcome(msg):
     bot.send_message(chat_id=msg.chat.uid, text="Howdy, how are you doing?", parse_mode=None, entities=None,
                      disable_web_page_preview=False, disable_notification=False, reply_to_message_id=msg.message_id,
                      allow_sending_without_reply=True, reply_markup=None)
 
 
-@bot.update_handler(update_type='message', regexp='hi')
+@bot.update_handler(regexp='hi')
 def send_hi(msg):
     bot.send_message(chat_id=msg.chat.uid, text=f'Hi 👋, {msg.from_user.first_name}', parse_mode=None, entities=None,
                      disable_web_page_preview=False, disable_notification=False, reply_to_message_id=None,
                      allow_sending_without_reply=True, reply_markup=None)
 
 
-@bot.update_handler(update_type='message', func=lambda message: message.text)
+@bot.update_handler(func=lambda message: message.text)
 def echo_all(msg):
     bot.send_message(chat_id=msg.chat.uid, text=msg.text, parse_mode=None, entities=None,
                      disable_web_page_preview=False, disable_notification=False, reply_to_message_id=None,
