bff970804a0aca14bd1ce12bfc1665a86ef1b21c
Mustafa Asaad <ma24th@hotmail.com>
2022-04-26 19:22:23 +0300

    Fix ApiException multi args*

--- tgbotapi/utils/api_exceptions.py
+++ tgbotapi/utils/api_exceptions.py
@@ -10,12 +10,8 @@ This submodule provides api exception objects that are consumed internally by tg
 class ApiException(Exception):
     """
     This class represents an Exception thrown when a call to the Telegram API fails.
-    In addition to an informative message, it has a `function_name` and a `result` attribute, which respectively
-    contain the name of the failed function and the returned result that made the function to be considered  as
-    failed.
     """
 
-    def __init__(self, msg, function_name, result):
-        super(ApiException, self).__init__(f"{msg}")
-        self.function_name = function_name
+    def __init__(self, text, result):
+        super(ApiException, self).__init__(f"{text}")
         self.result = result
