883b2c9d54da6cb455e06199074daae67805af28
Mustafa Asaad <ma24th@hotmail.com>
2022-05-07 01:29:39 +0300

    Improve imports

--- tgbotapi/__init__.py
+++ tgbotapi/__init__.py
@@ -3,10 +3,13 @@
 """
 tgbotapi Telegram Bot API Client Framework
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This module provides a bot client instance to implement all telegram bot api methods and types.
+This module contains the Python API client to the Telegram Bot API.
+
 :copyright: (c) 2022 by Mustafa Asaad.
 :license: GPLv2, see LICENSE for more details.
 """
+
 from .utils import logger
 from .asyncbot import AsyncBot
 from .bot import Bot
+from .types import *
