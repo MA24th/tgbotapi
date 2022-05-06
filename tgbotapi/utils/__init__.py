1e6f4057a9305b55f9d379e3291997c56884ff58
Mustafa Asaad <ma24th@hotmail.com>
2022-05-07 01:29:29 +0300

    Improve imports

--- tgbotapi/utils/__init__.py
+++ tgbotapi/utils/__init__.py
@@ -3,12 +3,15 @@
 """
 tgbotapi.utils
 ~~~~~~~~~~~~~~
-This submodule provides utility functions that are consumed internally by tgbotapi
+This module provides utility functions that are consumed internally
+by the other modules of tgbotapi.
+
 :copyright: (c) 2022 by Mustafa Asaad.
 :license: GPLv2, see LICENSE for more details.
 """
-from .api_exceptions import *
-from .api_handler import *
-from .api_worker import *
-from .json_helper import *
-from .logger import *
+
+from .api_exceptions import ApiException
+from .api_handler import make_request
+from .api_worker import ThreadWorker, ThreadPool, events_handler
+from .json_helper import JsonDeserializable, JsonSerializable
+from .logger import logger
