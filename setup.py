e8761e0ea466a90f632c744bee3cf5aea9af777a
Mustafa Asaad <ma24th@hotmail.com>
2022-05-08 13:58:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from io import open
+
 from setuptools import setup, find_packages
+
+from tgbotapi import __title__, __version__, __description__, __author__, __author_email__, __license__
 
 
 def read(filename):
@@ -8,24 +11,30 @@ def read(filename):
         return file.read()
 
 
-setup(name='tgbotapi',
-      version='5.7',
-      description='The Ultimate Telegram Bot API Framework',
+setup(name=__title__,
+      version=__version__,
+      description=__description__,
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
-      author='Mustafa Asaad',
-      author_email='ma24th@yahoo.com',
+      author=__author__,
+      author_email=__author_email__,
       url='https://github.com/MA24th/tgbotapi',
       packages=find_packages(),
-      license='GNU GPLv2',
+      classifiers=[
+          'Framework :: tgbotapi',
+          'Topic :: Software Development',
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: English'],
+      license=__license__,
       keywords='telegram-bot-api, tgbotapi, framework, telegram bot api, bot api',
       install_requires=['requests', 'six'],
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Framework :: tgbotapi'
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9']
+
       )
