607a5ea471d168073e68af76ab84a7db89ed925e
Mustafa Asaad <ma24th@hotmail.com>
2022-05-08 14:00:37 +0300

    Optimize event handler and exceptions

--- tgbotapi/utils/api_worker.py
+++ tgbotapi/utils/api_worker.py
@@ -3,17 +3,17 @@
 """
 tgbotapi.utils.api_worker
 ~~~~~~~~~~~~~~~~~~~~~~~~~
-This submodule provides threading utilities that are consumed internally by tgbotapi
+This module contains threading classes that are consumed internally by tgbotapi
 """
+
 import queue
-import sys
 import threading
 import traceback
 
-from . import logger, ApiException
+from .logger import logger
 
 
-class WorkerThread(threading.Thread):
+class ThreadWorker(threading.Thread):
     """
     This class is used internally by the bot to start polling and webhooks.
     """
@@ -34,13 +34,13 @@ class WorkerThread(threading.Thread):
         self.sequence = sequence
         self.daemon = True
 
+        self.event_received = threading.Event()
         self.event_exception = threading.Event()
         self.event_continue = threading.Event()
         self.event_completed = threading.Event()
-        self.event_received = threading.Event()
 
         self.exception_callback = exception_callback
-        self.exc_info = None
+        self.exception_info = None
         self._running = True
         self.start()
 
@@ -51,26 +51,27 @@ class WorkerThread(threading.Thread):
         while self._running:
             try:
                 task, args, kwargs = self.sequence.get(block=True, timeout=.5)
+                self.event_received.clear()
                 self.event_exception.clear()
                 self.event_continue.clear()
                 self.event_completed.clear()
-                self.event_received.clear()
 
                 logger.info("TASK RECEIVED")
                 self.event_received.set()
                 task(*args, **kwargs)
                 self.event_completed.set()
                 logger.info("TASK COMPLETED")
+
             except queue.Empty:
                 pass
-            except Exception or ApiException as e:
+
+            except Exception as e:
                 logger.error(f"TASK FAILED")
                 logger.error(f'{e} \n {traceback.format_exc()}')
-
-                self.exc_info = sys.exc_info()
+                self.exception_info = e  # sys.exc_info()
                 self.event_exception.set()
                 if self.exception_callback:
-                    self.exception_callback(self, self.exc_info)
+                    self.exception_callback(self, self.exception_info)
                 self.event_continue.wait()
 
     def put(self, task, *args, **kwargs):
@@ -84,7 +85,7 @@ class WorkerThread(threading.Thread):
         Raises exceptions if there are any
         """
         if self.event_exception.is_set():
-            raise Exception(self.exc_info)
+            raise self.exception_info
 
     def clear_exceptions(self):
         """
@@ -100,23 +101,23 @@ class WorkerThread(threading.Thread):
         self._running = False
 
 
-class PoolThread(object):
+class ThreadPool(object):
     """
     ThreadPool class that can be used to run tasks in parallel.
     """
 
-    def __init__(self, num_threads, sequence=queue.Queue()):
+    def __init__(self, max_workers, sequence=queue.Queue()):
         """
         Initializes a new ThreadPool
-        :param int num_threads: Number of threads to use in the pool
+        :param int max_workers: maximum number of workers
         :param sequence: Sequence to use for the thread
         """
         self.sequence = sequence
-        self.workers = [WorkerThread(f'', self.sequence, self.on_exception) for _ in range(num_threads)]
-        self.num_threads = num_threads
+        self.workers = [ThreadWorker(f'', self.sequence, self.on_exception) for _ in range(max_workers)]
+        self.max_workers = max_workers
 
         self.event_exception = threading.Event()
-        self.exc_info = None
+        self.exception_info = None
 
     def put(self, func, *args, **kwargs):
         """
@@ -124,20 +125,20 @@ class PoolThread(object):
         """
         self.sequence.put((func, args, kwargs))
 
-    def on_exception(self, worker_thread, exc_info):
+    def on_exception(self, worker_thread, exception_info):
         """
         Callback for exceptions in threads.
         """
-        self.exc_info = exc_info
+        self.exception_info = exception_info
         self.event_exception.set()
-        worker_thread.event_continue.set()
+        worker_thread.event_exception.set()
 
     def raise_exceptions(self):
         """
         Raises exceptions in threads.
         """
         if self.event_exception.is_set():
-            raise Exception(self.exc_info)
+            raise Exception(self.exception_info)
 
     def clear_exceptions(self):
         """
@@ -157,18 +158,21 @@ class PoolThread(object):
 
 def events_handler(event=threading.Event(), *events_status):
     """
-    Helper function to wait for an event to be set and check if the status is in the list of events_status.
+    Helper function to wait for an event to be set and check if the status is in the list of events_status
+    :param threading.Event() event: Event to wait for
+    :param list[Threading.Event()] events_status: List of events status to check
     """
+    event.busy_wait = event.wait
+
+    def busy_wait(n=3):
+        while not event.is_set():
+            event.busy_wait(n)
 
     def if_changed():
         if any([_.is_set() for _ in events_status]):
             event.set()
         else:
             event.clear()
-
-    def busy_wait():
-        while not event.is_set():
-            event.busy_wait(3)
 
     def or_set(x):
         x.t_set()
@@ -185,57 +189,6 @@ def events_handler(event=threading.Event(), *events_status):
         status.set = lambda: or_set(status)
         status.clear = lambda: or_clear(status)
 
-    event.busy_wait = event.wait
     event.wait = busy_wait
     if_changed()
     return event
-
-
-class AsyncTask:
-    """
-    AsyncTask class that can be used to run tasks in parallel.
-    """
-
-    def __init__(self, target, *args, **kwargs):
-        """
-        Initializes a new AsyncTask
-        :param target: The target function to run
-        :param args: The arguments to pass to the target function
-        :param kwargs: The keyword arguments to pass to the target function
-        """
-        self.target = target
-        self.args = args
-        self.kwargs = kwargs
-
-        self.done = False
-        self.thread = threading.Thread(target=self._run)
-        self.thread.start()
-
-    def _run(self):
-        try:
-            self.result = self.target(*self.args, **self.kwargs)
-        except AssertionError:
-            self.result = sys.exc_info()
-        self.done = True
-
-    def wait(self):
-        if not self.done:
-            self.thread.join()
-        if isinstance(self.result, BaseException):
-            raise ApiException(self.result)
-        else:
-            return self.result
-
-
-def async_dec():
-    """
-    Decorator that can be used to run a function asynchronously.
-    """
-
-    def decorator(fn):
-        def wrapper(*args, **kwargs):
-            return AsyncTask(fn, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
